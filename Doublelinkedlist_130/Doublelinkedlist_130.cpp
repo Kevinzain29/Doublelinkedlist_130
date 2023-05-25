ac6b6dadfc45729d34204a13266833b9183af5d8
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-05-26 00:59:40 +0700

    menambah fungsi while

--- Doublelinkedlist_130/Doublelinkedlist_130.cpp
+++ Doublelinkedlist_130/Doublelinkedlist_130.cpp
@@ -55,3 +55,11 @@ void DoubleLinkedList::addNode() {
 		return;
 	}
 
+	/*inserting a Node between two Nodes in the list*/
+	Node* current = START; // step 1.a
+	Node* previous = NULL; // step 1.b
+	while (current->next != NULL && current->next->noMhs < nim) // step 1.c
+	{
+		previous = current; // 1.d
+		current = current->next; // step 1.e
+	}
