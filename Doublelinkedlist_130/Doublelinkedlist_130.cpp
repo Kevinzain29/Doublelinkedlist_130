0c188bf852a30068c021e4727e8fa6599c32f6c3
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-05-26 01:07:34 +0700

    menambahkan fungsi int main

--- Doublelinkedlist_130/Doublelinkedlist_130.cpp
+++ Doublelinkedlist_130/Doublelinkedlist_130.cpp
@@ -168,3 +168,47 @@ void DoubleLinkedList::searchData() {
 		cout << "\nName: " << curr->name << endl;
 	}
 }
+
+int main() {
+	DoubleLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all records in the ascending order of roll numbers" << endl;
+			cout << "4. View all records in the descending order of the roll numbers" << endl;
+			cout << "5. search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): ";
+			char ch;
+			cin >> ch;
+
+			switch (ch) {
+			case '1':
+				obj.addNode();
+				break;
+			case'2':
+				obj.hapus();
+				break;
+			case'3':
+				obj.traverse();
+				break;
+			case'4':
+				obj.revtraverse();
+				break;
+			case'5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\ninvalid option" << endl;
+				break;
+			}
+		}
+		catch (exception& e) {
+			cout << "check for the values entered." << endl;
+		}
+	}
+}
